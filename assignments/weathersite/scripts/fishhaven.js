5da59aa59d5a6a8926af1c3528cab4fbe551a950
Ruggles-Jennifer <43383868+JeneralLee@users.noreply.github.com>
2018-11-25 01:58:58 -0600

    push for tonight

---
+++ assignments/weathersite/scripts/fishhaven.js
@@ -0,0 +1,26 @@
+var apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=42.0372&lon=-111.3960&units=imperial&APPID=69d6a88fe18541eb0984a7492251747e";
+var weatherRequest = new XMLHttpRequest();
+weatherRequest.open("GET", apiURL);
+weatherRequest.responseType = 'json';
+weatherRequest.send();
+weatherRequest.onload = function (){
+    var currentTemp = weatherRequest.response;
+    document.getElementById("weatherdescription").innerHTML = currentTemp.list[0].weather[0].main;
+    document.getElementById("current").innerHTML = currentTemp.list[0].weather[0].main;
+    document.getElementById("t").innerHTML = currentTemp.list[0].main.temp;
+    document.getElementById("humidity").innerHTML = currentTemp.list[0].main.humidity;
+   document.getElementById("precipitation").innerHTML = currentTemp.list[0].########;
+    document.getElementById("s").innerHTML = currentTemp.list[0].wind.speed;
+}
+
+var forecastURL = "api.openweathermap.org/data/2.5/forecast?lat=42.0372&lon=-111.3960&units=imperial&APPID=69d6a88fe18541eb0984a7492251747e";
+var forecastRequest = new XMLHttpRequest();
+forecastRequest.open("GET", forecastURL, "jsonp");
+forecastRequest.responseType = "json";
+forecastRequest.send();
+forecastRequest.onload = function (){
+  var forecast = forecastRequest.response;
+/*  document.getElementById("").innerHTML = */
+  console.log(weatherRequest.response);
+
+}
--- assignments/weathersite/scripts/preston.js
+++ assignments/weathersite/scripts/preston.js
@@ -1,17 +1,18 @@
-/*var apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=69d6a88fe18541eb0984a7492251747e";
+var apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=69d6a88fe18541eb0984a7492251747e";
 var weatherRequest = new XMLHttpRequest();
 weatherRequest.open("GET", apiURL);
 weatherRequest.responseType = 'json';
 weatherRequest.send();
 weatherRequest.onload = function (){
     var currentTemp = weatherRequest.response;
-    document.getElementById("weatherdescription").innerHTML = currentTemp.list[0].weather[0].main;
-    document.getElementById("current").innerHTML = currentTemp.list[0].weather[0].main;
+/*    document.getElementById("weatherdescription").innerHTML = currentTemp.list[0].weather[1].main;
+    document.getElementById("current").innerHTML = currentTemp.list[0].weather[0].main; */
     document.getElementById("t").innerHTML = currentTemp.list[0].main.temp;
-    document.getElementById("humidity").innerHTML = currentTemp.list[0].main.humidity;
-   document.getElementById("precipitation").innerHTML = currentTemp.list[0].########;
+/*   document.getElementById("humidity").innerHTML = currentTemp.list[0].main.humidity; 
+   document.getElementById("precipitation").innerHTML = currentTemp.list[0].main.precipitation; */
     document.getElementById("s").innerHTML = currentTemp.list[0].wind.speed;
-}*/
+    console.log(weatherRequest.response);
+}
 
 var forecastURL = "api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=69d6a88fe18541eb0984a7492251747e";
 var forecastRequest = new XMLHttpRequest();
@@ -21,6 +22,6 @@ forecastRequest.send();
 forecastRequest.onload = function (){
   var forecast = forecastRequest.response;
 /*  document.getElementById("").innerHTML = */
-  console.log(weatherRequest.response);
+  console.log(forecastRequest.response);
 
 }
